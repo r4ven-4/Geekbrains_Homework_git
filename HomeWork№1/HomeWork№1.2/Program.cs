4fa54e27e1cf03e425c06ab9a05a3ada2c57ccfe
Sergey <r4ven.4@yandex.ru>
2020-10-31 12:52:17 +0400

    Add HomeWork 1

---
+++ HomeWork№1/HomeWork№1.2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork_1._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите свой рост:");
+            int userHeight = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Введите свой вес:");
+            int userWeight = Convert.ToInt32(Console.ReadLine());
+
+            float IMT = userWeight / userHeight ^ 2;
+
+            Console.WriteLine("Ваш индекс массы тела равен: {0}", IMT);
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ HomeWork№1/HomeWork№1/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LibraryСsharpGeakbriensLessons;
+
+namespace HomeWork_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            
+
+            Console.Write("Введите свое имя:");
+            string userName = Console.ReadLine();
+
+            Console.Write("Введите свою фамилию:");
+            string userLastName = Console.ReadLine();
+
+            Console.Write("Введите свой возраст:");
+            int userAge = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Введите свой рост:");
+            int userHeight = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Введите свой вес:");
+            int userWeight = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Имя " + userName + " Фамилия " + userLastName + " Возраст " + userAge + " Рост " + userHeight + " Вес " + userWeight);
+
+            Console.WriteLine("Имя: {0}; Фамилия: {1}; Возраст: {2}; Рост:{3}; Вес: {4}.", userName, userLastName, userAge, userHeight, userWeight);
+
+            Console.WriteLine($"Имя: {userName}; Фамилия: {userLastName}; Возраст: {userAge}; Рост:{userHeight}; Вес: {userWeight}.");
+
+
+            Geek.Pause();
+
+        }
+
+       
+        
+
+    }
+}
