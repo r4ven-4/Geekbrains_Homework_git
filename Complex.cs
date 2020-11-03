f2689fdd9ed966a6deb43f191dc29703a5e3c2ac
Sergey <r4ven.4@yandex.ru>
2020-11-03 16:38:15 +0400

    Add HomeWork3

---
+++ Complex.cs
@@ -0,0 +1,52 @@
+﻿namespace HomeWork_3
+{
+    struct Complex
+    {
+        double b; // imaginary
+        double a; // real        
+        public Complex(double a,double b)
+        {
+            this.a = a;
+            this.b = b;
+        }
+        public Complex Plus(Complex x2)
+        {
+            Complex x3 = new Complex();
+            x3.b = x2.b + this.b;
+            x3.a = x2.a + this.a;
+            return x3;
+        }
+
+        public Complex Minus(Complex x2)
+        {
+            Complex x3 = new Complex();
+            x3.b = x2.b - this.b;
+            x3.a = x2.a - this.a;
+            return x3;
+        }
+
+
+        public Complex Multi(Complex x2)
+        {
+            Complex x3 = new Complex();
+            x3.b = this.a * x2.b + this.b * x2.a;
+            x3.a = this.a * x2.a - this.b * x2.b;
+            return x3;
+        }
+        public string ToString()
+        {
+            return a + " " + b + "i";
+        }
+
+        public double B
+        {
+            get { return b; }
+            set
+            {
+                if (value >= 0) b = value;
+            }
+        }
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LibraryСsharpGeekbrainsLessons;
+
+namespace HomeWork_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Complex complex1 = new Complex(1, 1);
+
+            Complex complex2 = new Complex(2, 2);
+
+
+            Complex result = complex1.Multi(complex2);
+            Console.WriteLine(result.ToString());
+            Geek.Pause();
+
+            Console.WriteLine(SumOddNumber());
+
+            Geek.Pause();
+
+        }
+
+        public static int SumOddNumber()
+        {
+            int Sum = 0, Num;
+
+            while (true)
+            {
+                Num = GetNumber();
+
+                if (Num == 0) { break; }
+                else if (Num % 2 == 1 && Num > 0) { Sum += Num; }
+            }
+
+            return Sum;
+        }
+
+        private static int GetNumber()
+        {
+            int Num;
+            bool flag;
+            do
+            {            
+                Console.Write("Введите число: ");
+                flag = int.TryParse(Console.ReadLine(), out Num);
+
+                if (!flag) { Console.WriteLine("\nОШИБКА! ВВЕДИТЕ ЧИСЛО!!!"); }
+
+            } while (!flag);
+
+            
+            return Num;
+        }
+    }
+}
